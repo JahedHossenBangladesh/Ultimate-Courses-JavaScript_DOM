6341779a381c037f613e1e2d515081d16f8e9f48
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-09-05 22:12:14 +0600

    Creating Dom Element

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
 import '../assets/css/style.css';
 
+
 const app = document.getElementById('app');
+const div = document.createElement('div');
+const text = document.createTextNode('DOM!')
+const comment = document.createComment('This is comment')
 
-app.innerHTML = `
-<h1>JavaScript DOM</h1>
-`;
-// html
-console.log(document.documentElement)
-console.dir(document.documentElement)
 
-//head
-console.dir(document.head);
-//body
-console.dir(document.body)
-
-//retrieve the constructor name
-console.log(document.body.constructor.name)
-
-//looking at the prototype changes 
-console.log(document.body instanceof HTMLBodyElement)
-console.log(document.body instanceof HTMLElement)
-console.log(document.body instanceof Element)
-console.log(document.body instanceof Node)
-console.log(document.body instanceof EventTarget)
-
-console.log(document.body.nodeType)
-console.log(document.nodeType)
-//nodeName for any Node types
-console.log(document.nodeName)
-// tagName for any Element types
-console.log(document.body.tagName)
+div.append(comment);
+div.append(text);
+app.append(div);
+console.log(app,div);
+console.log( div);
