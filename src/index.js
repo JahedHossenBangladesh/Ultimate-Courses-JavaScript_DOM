bbf2b3a98a66fbf3be13cfde07881d682608c09b
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-09-07 01:15:24 +0600

    inserting DOM as String Template

--- src/index.js
+++ src/index.js
@@ -5,20 +5,32 @@ const app = document.getElementById('app');
 
 app.innerHTML = `
 <h1>Javascript DOM</h1>
+<ul>
+<li>i</li>
+</ul>
 `
-//inserting in DOM
-const div = document.createElement('div');
-const li = document.createElement('li');
-const p = document.createElement('p');
-const span = document.createElement('span');
-const i = document.createElement('i');
-const b = document.createElement('b');
+const ul = app.querySelector('ul');
 
-div.append(span)
-div.prepend(p)
-span.after(i)
-p.before(li)
-console.log(div)
+ul.insertAdjacentHTML('beforebegin','<p> Before</p>')
+ul.insertAdjacentHTML('afterbegin','<li>After Begin</li>')
+ul.insertAdjacentHTML('beforeend','<li>BeforeEnd</li>')
+ul.insertAdjacentHTML("afterend", "<p>AfterEnd</p>");
+
+
+
+//inserting in DOM
+// const div = document.createElement('div');
+// const li = document.createElement('li');
+// const p = document.createElement('p');
+// const span = document.createElement('span');
+// const i = document.createElement('i');
+// const b = document.createElement('b');
+
+// div.append(span)
+// div.prepend(p)
+// span.after(i)
+// p.before(li)
+// console.log(div)
 
 
 
