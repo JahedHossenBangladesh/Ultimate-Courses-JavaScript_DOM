e472a822132e68ff98d0e1fb1efd485e49ff4373
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-09-07 00:55:43 +0600

    inserting in DOM

--- src/index.js
+++ src/index.js
@@ -5,17 +5,40 @@ const app = document.getElementById('app');
 
 app.innerHTML = `
 <h1>Javascript DOM</h1>
-
-
 `
+//inserting in DOM
+const div = document.createElement('div');
+const li = document.createElement('li');
+const p = document.createElement('p');
+const span = document.createElement('span');
+const i = document.createElement('i');
+const b = document.createElement('b');
 
-const data = ['apple','banana','pineapple','more']
+div.append(span)
+div.prepend(p)
+span.after(i)
+p.before(li)
+console.log(div)
 
-const fragment = document.createDocumentFragment();
-data.forEach(name => {
-    const li = document.createElement('li');
-    li.innerText =name;
-    fragment.append(li)
-})
 
-app.append(fragment)
+
+
+
+
+
+
+
+
+
+
+// // fragement
+// const data = ['apple','banana','pineapple','more']
+
+// const fragment = document.createDocumentFragment();
+// data.forEach(name => {
+//     const li = document.createElement('li');
+//     li.innerText =name;
+//     fragment.append(li)
+// })
+
+// app.append(fragment)
