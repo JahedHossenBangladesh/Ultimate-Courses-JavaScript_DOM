3f7dbbd9655e78fdfb5aa6b7bd682ea3d0ffd234
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-09-06 19:18:25 +0600

    innerHTML versus createElement

--- src/index.js
+++ src/index.js
@@ -3,18 +3,37 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 
-// app.innerHTML = `
-// <h1>Javascript DOM</h1>
-// `
-// app.innerText = 'some text here'
+app.innerHTML = `
+<h1>Javascript DOM</h1>
 
-const h1 = document.createElement('h1')
 
-h1.innerText = 'Javascript DOM'
-h1.innerText += ' Ultimate JS'
+`
+// createElement
+function createInput({label,type='text'}){
+    const labelEl= document.createElement('label');
+    const inputEl = document.createElement('input');
+    inputEl.type = type;
+    labelEl.innerText = label;
+    labelEl.append(inputEl)
+    return labelEl;
+}
+const inputFromDom = createInput({label:"name"})
 
-app.append(h1);
+app.append(inputFromDom);
+console.log(inputFromDom)
+// string template 
+function createInput2({label,type='text'}){
+    return `
+    <label>
+    ${label}
+    <input type="${type}" placeholder="${label}"/>
+    </label>
+    
+    `
+}
+const inputFromDom2 = createInput2({
+    label:'Email',
+    type:'email'
+})
 
-console.log(app.innerHTML)
-console.log(app.innerText)
-console.log(app.textContent)
+app.innerHTML += inputFromDom2;
